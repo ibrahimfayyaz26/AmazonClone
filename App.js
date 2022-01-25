0730f19e5c809137557b6d95588b065b5b81fce8
Ibrahim <79158349+ibrahimfayyaz26@users.noreply.github.com>
2022-01-25 17:47:15 +0500

    1.4

--- App.js
+++ App.js
@@ -9,10 +9,7 @@
 import React from 'react';
 import {SafeAreaView, StatusBar, useColorScheme} from 'react-native';
 import {Colors} from 'react-native/Libraries/NewAppScreen';
-import Address from './src/screens/Address/Address';
-import Cart from './src/screens/Cart/Cart';
-import Home from './src/screens/Home/Home';
-import Product from './src/screens/Product/Product';
+import Navigation from './src/router';
 
 const App = () => {
   const isDarkMode = useColorScheme() === 'dark';
@@ -24,7 +21,7 @@ const App = () => {
   return (
     <SafeAreaView style={backgroundStyle}>
       <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <Address />
+      <Navigation />
     </SafeAreaView>
   );
 };
--- src/screens/Address/Address.js
+++ src/screens/Address/Address.js
@@ -1,5 +1,13 @@
 import React, {useState} from 'react';
-import {View, Text, TextInput, Alert} from 'react-native';
+import {
+  View,
+  Text,
+  TextInput,
+  Alert,
+  ScrollView,
+  KeyboardAvoidingView,
+  Platform,
+} from 'react-native';
 import styles from './styles';
 import {Picker} from '@react-native-picker/picker';
 import countryList from 'country-list';
@@ -16,67 +24,94 @@ const Address = () => {
   const [city, setCity] = useState('');
 
   const onCheckout = () => {
-    console.warn('Hello');
+    if (addressError) {
+      Alert.alert('Fix all field errors before submiting');
+      return;
+    }
+
+    if (!fullname) {
+      Alert.alert('Please fill in the fullname field');
+      return;
+    }
+
+    if (!phone) {
+      Alert.alert('Please fill in the phone number field');
+      return;
+    }
+  };
+
+  const validateAddress = () => {
+    if (address.length < 3) {
+      setAddressError('Address is too short');
+    }
   };
 
   return (
-    <View style={styles.root}>
-      <View style={styles.container}>
-        <Picker selectedValue={country} onValueChange={setCountry}>
-          {countries.map(op => (
-            <Picker.Item key={op.code} label={op.name} value={op.code} />
-          ))}
-        </Picker>
-      </View>
-      {/* Full name */}
-      <View style={styles.row}>
-        <Text style={styles.label}>Full name (First and Last name)</Text>
-        <TextInput
-          style={styles.input}
-          placeholder="Full name"
-          value={fullname}
-          onChangeText={setFullname}
-        />
-      </View>
+    <KeyboardAvoidingView
+      behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+      keyboardVerticalOffset={Platform.OS === 'ios' ? 10 : 0}>
+      <ScrollView style={styles.root}>
+        <View style={styles.container}>
+          <Picker selectedValue={country} onValueChange={setCountry}>
+            {countries.map(op => (
+              <Picker.Item key={op.code} label={op.name} value={op.code} />
+            ))}
+          </Picker>
+        </View>
+        {/* Full name */}
+        <View style={styles.row}>
+          <Text style={styles.label}>Full name (First and Last name)</Text>
+          <TextInput
+            style={styles.input}
+            placeholder="Full name"
+            value={fullname}
+            onChangeText={setFullname}
+          />
+        </View>
 
-      {/* Phone number */}
-      <View style={styles.row}>
-        <Text style={styles.label}>Phone number</Text>
-        <TextInput
-          style={styles.input}
-          placeholder="Phone number"
-          value={phone}
-          onChangeText={setPhone}
-          keyboardType={'phone-pad'}
-        />
-      </View>
+        {/* Phone number */}
+        <View style={styles.row}>
+          <Text style={styles.label}>Phone number</Text>
+          <TextInput
+            style={styles.input}
+            placeholder="Phone number"
+            value={phone}
+            onChangeText={setPhone}
+            keyboardType={'phone-pad'}
+          />
+        </View>
 
-      {/* Address */}
-      <View style={styles.row}>
-        <Text style={styles.label}>Address</Text>
-        <TextInput
-          style={styles.input}
-          placeholder="Address"
-          value={address}
-          onChangeText={text => {
-            setAddress(text);
-            setAddressError('');
-          }}
-        />
-      </View>
+        {/* Address */}
+        <View style={styles.row}>
+          <Text style={styles.label}>Address</Text>
+          <TextInput
+            style={styles.input}
+            placeholder="Address"
+            value={address}
+            onEndEditing={validateAddress}
+            onChangeText={text => {
+              setAddress(text);
+              setAddressError('');
+            }}
+          />
+          {!!addressError && (
+            <Text style={styles.errorLabel}>{addressError}</Text>
+          )}
+        </View>
 
-      {/* City */}
-      <View style={styles.row}>
-        <Text style={styles.label}>City</Text>
-        <TextInput
-          style={styles.input}
-          placeholder="City"
-          value={city}
-          onChangeText={setCity}
-        />
-      </View>
-      <ButtonCo text="Checkout" press={onCheckout} />
-    </View>
+        {/* City */}
+        <View style={styles.row}>
+          <Text style={styles.label}>City</Text>
+          <TextInput
+            style={styles.input}
+            placeholder="City"
+            value={city}
+            onChangeText={setCity}
+          />
+        </View>
+        <ButtonCo text="Checkout" press={onCheckout} />
+      </ScrollView>
+    </KeyboardAvoidingView>
   );
 };
 
